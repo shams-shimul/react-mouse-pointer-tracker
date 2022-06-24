01e34bcec76e105f97befd3dbe09858f44abeef7
shams-shimul <msr.shimul@gmail.com>
2022-06-25 01:12:04 +0600

    1 file modified

--- src/Text.js
+++ src/Text.js
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 
 export const Text = memo(() => {
-  console.log('Text rendered');
+  // console.log('Text rendered');
 
   return <h1>Try to move your mouse over here</h1>;
 });
